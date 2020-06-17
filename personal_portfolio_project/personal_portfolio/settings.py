e932caeed53540c427a996db6b1d5be1f5f3c9bd
Jairus Elarbee <jelarbee@gmail.com>
2020-06-17 10:13:34 -0400

    Made it parallel website

--- personal_portfolio_project/personal_portfolio/settings.py
+++ personal_portfolio_project/personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'etvu2#%5beoqoap0&w01er^7hp1k@dnw+!2-z8x^7n(dno$so4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jelarbee.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
